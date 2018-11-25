ed18b3ccba4ea391676d1da6defb75a061a3a10b
udf <tabhooked@gmail.com>
2018-11-25 02:45:10 +0200

    Add check for large files and comments

--- stdplugins/kbass_file_to_photo.py
+++ stdplugins/kbass_file_to_photo.py
@@ -14,8 +14,12 @@ async def on_file_to_photo(event, target):
         image = target.media.document
     except AttributeError:
         return
-    if image.mime_type == 'image/webp' or not image.mime_type.startswith('image/'):
-        return
+    if not image.mime_type.startswith('image/'):
+        return  # This isn't an image
+    if image.mime_type == 'image/webp':
+        return  # Telegram doesn't let you directly send stickers as photos
+    if image.size > 10 * 1024 * 1024:
+        return  # We'd get PhotoSaveFileInvalidError otherwise
 
     file = await borg.download_media(target, file=BytesIO())
     file.seek(0)
