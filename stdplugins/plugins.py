d18b85a709c8cc7eaf26fa31fc92f9a379be0854
Dan Elkouby <streetwalkermc@gmail.com>
2020-04-06 14:50:05 +0000

    stdplugins/plugins: fix

--- stdplugins/plugins.py
+++ stdplugins/plugins.py
@@ -5,7 +5,8 @@ from telethon import events
 
 
 @borg.on(events.NewMessage(pattern=r"\.plugins", outgoing=True))
-async def _(event): result = f'{len(borg._plugins)} plugins loaded:'
+async def _(event):
+    result = f'{len(borg._plugins)} plugins loaded:'
     for name, mod in sorted(borg._plugins.items(), key=lambda t: t[0]):
         desc = (mod.__doc__ or '__no description__').replace('\n', ' ').strip()
         result += f'\n**{name}**: {desc}'
