da6b7dbc7d92b1022e43b7f24fffbfe462e9e3fd
udf <tabhooked@gmail.com>
2020-08-07 19:27:19 +0200

    add devborg.py

---
+++ devborg.py
@@ -0,0 +1,20 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import logging
+
+from uniborg import Uniborg
+import api_key
+
+logging.basicConfig(level=logging.DEBUG)
+
+borg = Uniborg(
+    "stdborg",
+    plugin_path="stdplugins_dev",
+    connection_retries=None,
+    api_id=api_key.id,
+    api_hash=api_key.hash
+)
+
+borg.run_until_disconnected()
