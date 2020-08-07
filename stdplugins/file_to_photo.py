e4e8822055f4eddaf2aeddddeba762d291a1ebea
udf <tabhooked@gmail.com>
2020-08-07 22:02:42 +0200

    Update file_to_photo
    * Remove size limit (using Pillow to resize)
    * Add .fd command to delete original message

--- stdplugins/file_to_photo.py
+++ stdplugins/file_to_photo.py
@@ -1,16 +1,33 @@
 """
 Reply to a file with .f to send it as a photo
+Use .fd to delete the file (only works if you sent the file)
 """
+import asyncio
 from io import BytesIO
+import concurrent.futures
 
-from uniborg import util
+from PIL import Image
 
-from telethon import types
 from telethon.errors import PhotoInvalidDimensionsError
-from telethon.tl.functions.messages import SendMediaRequest
 
 
-@borg.on(borg.admin_cmd(r"^\.f$"))
+executor = concurrent.futures.ThreadPoolExecutor()
+
+
+# From: https://github.com/Qwerty-Space/tanyabot/blob/d481c43baceffa2600aee950b33b23e52e10aa2c/plugins/global_functions.py
+def downscale(fp, max_w=1280, max_h=1280, format="PNG"):
+    im = Image.open(fp)
+    resolution = im.size
+    outfile = BytesIO()
+
+    im.thumbnail((max_w, max_h), Image.LANCZOS)
+    im.save(outfile, format)
+    outfile.seek(0)
+
+    return outfile
+
+
+@borg.on(borg.admin_cmd(r"f(d)?"))
 async def on_file_to_photo(event):
     await event.delete()
     target = await event.get_reply_message()
@@ -19,24 +36,26 @@ async def on_file_to_photo(event):
     except AttributeError:
         return
     if not image.mime_type.startswith('image/'):
-        return  # This isn't an image
-    if image.mime_type == 'image/webp':
-        return  # Telegram doesn't let you directly send stickers as photos
-    if image.size > 10 * 1024 * 1024:
-        return  # We'd get PhotoSaveFileInvalidError otherwise
+        return
 
     file = await borg.download_media(target, file=BytesIO())
     file.seek(0)
-    img = await borg.upload_file(file)
-    img.name = 'image.png'
+    im = await asyncio.get_running_loop().run_in_executor(
+        executor,
+        lambda: downscale(file)
+    )
+
+    im.name = "image.png"
+    should_delete = (target.from_id == borg.uid and event.pattern_match.group(1))
 
     try:
-        await borg(SendMediaRequest(
-            peer=await event.get_input_chat(),
-            media=types.InputMediaUploadedPhoto(img),
-            message=target.message,
-            entities=target.entities,
-            reply_to_msg_id=target.id
-        ))
+        await borg.send_message(
+            await event.get_input_chat(),
+            file=im,
+            reply_to=None if should_delete else target
+        )
+
+        if should_delete:
+            await target.delete()
     except PhotoInvalidDimensionsError:
         return
