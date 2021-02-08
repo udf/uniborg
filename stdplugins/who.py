ff262320b1b187c13ab790987cdac7f1a03b822d
Qwerty-Space <oliver_duce@live.co.uk>
2021-02-08 00:06:30 +0000

    use chat_id instead

--- stdplugins/who.py
+++ stdplugins/who.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
-Show information about the chat or replied user
+Show information about the chat or replied user using `.who`, `.members`, and `.active_members`
 """
 import datetime
 import html
@@ -13,17 +13,23 @@ from telethon import utils
 from telethon.tl import types, functions
 
 
-def get_who_string(who, rank=None):
-    who_string = html.escape(utils.get_display_name(who))
+def get_who_string(who, who_id=None, rank=None):
+    if who_id is None or isinstance(who, types.User):
+        who_id = who.id
+
+    who_string = f"<a href='tg://user?id={who.id}'>"
+    who_string += html.escape(utils.get_display_name(who)) + "</a>"
+
     if rank is not None:
         who_string += f' <i>"{html.escape(rank)}"</i>'
     if isinstance(who, (types.User, types.Channel)) and who.username:
         who_string += f" <i>(@{who.username})</i>"
-    who_string += f", <a href='tg://user?id={who.id}'>#{who.id}</a>"
+
+    who_string += f", <code>{who_id}</code>"
     return who_string
 
 
-@borg.on(events.NewMessage(pattern=r"\.who", outgoing=True))
+@borg.on(borg.cmd(r"who"))
 async def _(event):
     rank = None
     if not event.message.is_reply:
@@ -31,7 +37,7 @@ async def _(event):
     else:
         msg = await event.message.get_reply_message()
         if msg.forward:
-          	# FIXME forward privacy memes
+            # FIXME forward privacy memes
             who = await borg.get_entity(
                 msg.forward.from_id or msg.forward.channel_id)
         else:
@@ -43,10 +49,10 @@ async def _(event):
                     who
                 ))).participant, 'rank', None)
 
-    await event.edit(get_who_string(who, rank), parse_mode='html')
+    await event.respond(get_who_string(who, event.chat_id, rank), parse_mode='html')
 
 
-@borg.on(events.NewMessage(pattern=r"\.members", outgoing=True))
+@borg.on(borg.cmd(r"members"))
 async def _(event):
     last = 0
     index = 0
@@ -76,7 +82,7 @@ async def _(event):
     await event.edit("\n".join(members), parse_mode='html')
 
 
-@borg.on(events.NewMessage(pattern=r"\.active_members", outgoing=True))
+@borg.on(borg.cmd(r"active_members"))
 async def _(event):
     members = []
     async for member in borg.iter_participants(event.chat_id):
