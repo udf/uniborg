5cdfdebecb9bf438a61b1e12f64c7b2e7809864b
Qwerty-Space <oliver_duce@live.co.uk>
2021-02-15 21:34:29 +0000

    make a draft on rejoining the group

--- stdplugins/auto_rejoin.py
+++ stdplugins/auto_rejoin.py
@@ -4,12 +4,31 @@
 
 from asyncio import sleep, ensure_future
 
+from telethon import events
+from telethon.tl.functions.messages import SaveDraftRequest
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.errors import ChannelPrivateError, ChannelBannedError
+
+import logging
+logging.basicConfig(level=logging.WARNING)
 
 # How often to fetch the full list of blocked users
 REFETCH_TIME = 60 * 2
 chat = "@telethonofftopic"
+
+
+@borg.on(events.NewMessage(chats=chat))
+async def last_msg_draft(event):
+    last_msg = event.id
+    storage.last_msg = last_msg
+
+
+@borg.on(events.ChatAction(chats=chat, func=lambda e: e.user_joined))
+async def joined(event):
+    if event.user_id != borg.uid:
+        return
+    last_msg = storage.last_msg or 0
+    await borg(SaveDraftRequest(peer=chat, message="", reply_to_msg_id=last_msg))
 
 
 async def fetch_chats():
@@ -17,6 +36,7 @@ async def fetch_chats():
         try:
             await borg(JoinChannelRequest(chat))
         except (ChannelBannedError, ChannelPrivateError):
+            logger.info("Still banned")
             await sleep(REFETCH_TIME + 60)
         await sleep(REFETCH_TIME)
 
