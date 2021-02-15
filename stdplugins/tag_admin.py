7d54d4b2b048693f61828f608d96824fba623a29
Qwerty-Space <oliver_duce@live.co.uk>
2021-02-15 14:20:27 +0000

    don't tag bots

--- stdplugins/tag_admin.py
+++ stdplugins/tag_admin.py
@@ -12,6 +12,8 @@ async def _(event):
     reply_msg = "@admin"
 
     async for a in borg.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+        if a.bot:
+            continue
         reply_msg += f"[\u2063](tg://user?id={a.id})"
 
     await event.respond(reply_msg, reply_to=event.reply_to_msg_id)
