bb29cec5bcc143f77569f8d593ca3e6b25705ba5
novenary <streetwalkermc@gmail.com>
2022-01-01 15:26:37 +0000

    Rewrite pixiv plugin
    Now supports galleries!

--- stdplugins/pixiv_sucks.py
+++ stdplugins/pixiv_sucks.py
@@ -5,34 +5,52 @@
 Download images from pixiv links and post them directly
 """
 
+import asyncio
+
 from telethon import events
 
 import aiohttp
 
-# Unfortunately we can't guess the timestamp without parsing pixiv's HTML
-#@borg.on(events.NewMessage(outgoing=True,
-#    pattern=r"^https://www\.pixiv\.net/en/artworks/(?P<gallery>\d{8})#big_(?P<idx>\d+)$"))
+# Web gallery links, including those referring to a specific image
 @borg.on(events.NewMessage(outgoing=True,
-    pattern=r"^https://i\.pximg\.net/img-original/img/(?P<path>\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/(?P<gallery>\d{8})_p\d+)\.(?:png|jpg)$"))
+    pattern=r"^https://www\.pixiv\.net/(?:\w+)/artworks/(?P<gallery>\d{8})(?:#big_(?P<index>\d+))?$"))
+# Direct links to an image on the CDN
 @borg.on(events.NewMessage(outgoing=True,
-    pattern=r"^https://i\.pximg\.net/c/\w+/img-master/img/(?P<path>\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/(?P<gallery>\d{8})_p\d+)_master\d+\.jpg$"))
+    pattern=r"^https://i\.pximg\.net/.*/(?P<gallery>\d{8})_p(?P<index>\d+)(?:\w+)?\.(?:png|jpg)$"))
 async def _(event):
     gallery_id = event.pattern_match.group("gallery")
-    image_path = event.pattern_match.group("path")
+    image_index = event.pattern_match.group("index")
 
-    headers = { "Referer": "https://www.pixiv.net/" }
     async with aiohttp.ClientSession() as session:
-        for ext in ["png", "jpg"]:
-            image_url = f"https://i.pximg.net/img-original/img/{image_path}.{ext}"
-            async with session.get(image_url, headers=headers) as response:
-                if response.status == 404:
-                    continue
-                image = await response.read()
-                break
-        else:
+        async with session.get(f"https://www.pixiv.net/ajax/illust/{gallery_id}/pages") as response:
+            index = await response.json()
+        if index["error"]:
+            # TODO error message?
             return
-    await event.respond(
-        f"https://www.pixiv.net/en/artworks/{gallery_id}",
-        file=image
-    )
-    await event.delete()
+        urls = [i["urls"]["regular"] for i in index["body"]]
+        total = len(urls)
+
+        # If a specific image was linked, only send that
+        if image_index is not None:
+            urls = [urls[int(image_index)]]
+
+        # Limit to 10 images (a single Telegram album)
+        urls = urls[:10]
+
+        more = total - len(urls)
+        more = f"({more} more)" if more > 0 else ""
+        spinner = await borg.upload_file("spinner.jpg")
+        messages = await event.respond(
+            f"https://www.pixiv.net/en/artworks/{gallery_id} {more}",
+            file=[spinner] * len(urls)
+        )
+        await event.delete()
+
+        # This is required for the pixiv CDN to actually give us the images
+        headers = { "Referer": "https://www.pixiv.net/" }
+        for u, m in zip(urls, messages):
+            async with session.get(u, headers=headers) as response:
+                file = await response.read()
+                # Upload the image asynchronously so we can keep downloading
+                # concurrently
+                asyncio.create_task(m.edit(file=file))
